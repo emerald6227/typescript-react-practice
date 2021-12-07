e0c6d8dbf742e9d897db0ae42856014d29474c6d
emerald6227 <emerald6227@naver.com>
2021-12-07 14:21:50 +0900

    Optional Props practice

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps { // Container div가 받을 props객체 인터페이스
     bgColor: string;
+    borderColor: string;
 }
 
 // <ContainerProps> 를 추가해서 객체타입 지정
@@ -10,14 +11,21 @@ const Container = styled.div<ContainerProps>`
     height: 200px;
     background-color: ${props => props.bgColor};
     border-radius: 50%;
+    border: 1px solid ${props => props.borderColor};
 `;
 
 interface CircleProps { // Circle 컴포넌트의 props객체 인터페이스
     bgColor: string;
+    borderColor?: string; // Optional Props (string or undefined)
+    text?: string;
 }
 
-function Circle({bgColor}: CircleProps) { // props를 객체타입으로 지정
-    return <Container bgColor={bgColor}/>;
+function Circle({bgColor, borderColor, text = "default text"}: CircleProps) { // props를 객체타입으로 지정
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
+    );
 }
 
 // 아래처럼 props: CirlceProps로도 사용이 가능하다.
